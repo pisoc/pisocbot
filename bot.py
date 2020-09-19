3d734f897383caffbb48ce65fa961630408d8e7c
aphe-lion <tom.0505@hotmail.co.uk>
2020-09-19 23:08:56 +0100

    Change from polling to webhook updating

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import os
 import telegram.ext as ext
 
+TELEGRAM_API_TOKEN = os.getenv('PISOCBOT_TOKEN')
+PORT = int(os.getenv('PISOCBOT_PORT'))
+
 
 def hello_handler(update, context):
     update.message.reply_text('Hello there!')
@@ -8,10 +11,15 @@ def hello_handler(update, context):
 
 if __name__ == '__main__':
     updater = ext.Updater(
-        token=os.getenv('PISOCBOT_TOKEN'),
-        use_context=True
-    )
-    dispatcher = updater.dispatcher
+        token=TELEGRAM_API_TOKEN, use_context=True)
 
+    dispatcher = updater.dispatcher
     dispatcher.add_handler(ext.CommandHandler('hello', hello_handler))
-    updater.start_polling()
+
+    updater.start_webhook(
+        listen='0.0.0.0', port=PORT, url_path=TELEGRAM_API_TOKEN)
+    updater.bot.setWebhook(
+        'https://pisocbot.herokuapp.com/' + TELEGRAM_API_TOKEN)
+
+    updater.idle()
+
